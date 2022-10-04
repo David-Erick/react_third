4fcee17300b9e0921b4e226dc91e988255ab3b2a
David-Erick <108961323+David-Erick@users.noreply.github.com>
2022-10-04 20:36:27 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//components
+import FirstComponent from './components/FirstComponent';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>David Erick</h1>
+    <FirstComponent curso='softex'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FirstComponent.js
@@ -0,0 +1,10 @@
+const FirstComponent = (props) =>{
+    return(
+        <div>
+            <h1>{props.curso}</h1>
+        </div>
+
+    );       
+};
+
+export default FirstComponent; 
